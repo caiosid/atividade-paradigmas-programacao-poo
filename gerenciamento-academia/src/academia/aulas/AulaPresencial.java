aaccb8a6c93547396eabc7f0ea19b098d7798805
caiosid <caiosidartha8@gmail.com>
2024-10-29 10:40:13 -0300

    Aula Presencial

---
+++ gerenciamento-academia/src/academia/aulas/AulaPresencial.java
@@ -0,0 +1,9 @@
+package academia.aulas;
+
+public class AulaPresencial extends Aula{
+    
+    public AulaPresencial(int capacidadeMaxima, String instrutor, String nome) {
+        super(capacidadeMaxima, instrutor, nome);
+    }
+    
+}
