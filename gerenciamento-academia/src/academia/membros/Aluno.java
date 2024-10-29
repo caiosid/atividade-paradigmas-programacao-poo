1eade130a8e879e720ab0cb94887930a518049d1
caiosid <caiosidartha8@gmail.com>
2024-10-29 09:58:51 -0300

    Professor

---
+++ gerenciamento-academia/src/academia/membros/Aluno.java
@@ -0,0 +1,9 @@
+package academia.membros;
+
+public class Aluno extends Membro{
+    
+    public Aluno(String nome, int id, boolean pagamentoEmDia) {
+        super(nome, id, pagamentoEmDia);
+    }
+    
+}
---
+++ gerenciamento-academia/src/academia/membros/Professor.java
@@ -0,0 +1,5 @@
+package academia.membros;
+
+public class Professor {
+    
+}
