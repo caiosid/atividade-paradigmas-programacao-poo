888cb931a453baa93977e99b829b00029a95e21f
caiosid <caiosidartha8@gmail.com>
2024-10-29 10:01:01 -0300

    Aluno

--- gerenciamento-academia/src/academia/membros/Aluno.java
+++ gerenciamento-academia/src/academia/membros/Aluno.java
@@ -1,9 +1,8 @@
 package academia.membros;
 
 public class Aluno extends Membro{
-    
+
     public Aluno(String nome, int id, boolean pagamentoEmDia) {
         super(nome, id, pagamentoEmDia);
-    }
-    
+    }  
 }
