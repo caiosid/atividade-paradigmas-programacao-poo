86cec9fbb38d1f673e6884c37bd791e7ea460499
caiosid <caiosidartha8@gmail.com>
2024-10-29 06:02:53 -0300

    Classe Aulas

---
+++ gerenciamento-academia/src/academia/Aulas.java
@@ -0,0 +1,56 @@
+package academia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aulas {
+    private String nome;
+    private String instrutor;
+    private int capacidadeMaxima;
+    private List<Membro> listaMembrosInscritos;
+
+    public Aulas(int capacidadeMaxima, String instrutor, String nome) {
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.instrutor = instrutor;
+        this.nome = nome;
+        this.listaMembrosInscritos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public List<Membro> getListaMembrosInscritos() {
+        return listaMembrosInscritos;
+    }
+
+    public boolean inscreverMembro(Membro membro){
+        if(membro.isPagamentoEmDia() && verificarDisponibilidade()){
+            listaMembrosInscritos.add(membro);
+            return  true;
+        }
+        return false;
+    }
+
+    public boolean verificarDisponibilidade(){
+        return listaMembrosInscritos.size() < capacidadeMaxima;
+    }
+
+    public void listarMembrosInscritos(){
+        System.out.println("Membros inscritos na aula " + nome + ":");
+        for(Membro membro : listaMembrosInscritos){
+            System.out.println("- " + membro.getNome());
+        }
+    }
+
+    
+    
+}
