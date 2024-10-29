a7c2476e265dc7f8b525f1d28169427e5ed15983
caiosid <caiosidartha8@gmail.com>
2024-10-29 10:43:53 -0300

    Aula Online

---
+++ gerenciamento-academia/src/academia/aulas/AulaOnline.java
@@ -0,0 +1,9 @@
+package academia.aulas;
+
+public class AulaOnline extends Aula {
+    
+    public AulaOnline(int capacidadeMaxima, String instrutor, String nome) {
+        super(capacidadeMaxima, instrutor, nome);
+    }
+    
+}
